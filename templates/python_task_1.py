3d4d0422ea43ca41459085457dd500db4389de71
arpita-bansal <85424734+arpita-bansal@users.noreply.github.com>
2023-12-11 20:44:00 +0530

    Update python_task_1.py

--- templates/python_task_1.py
+++ templates/python_task_1.py
@@ -2,91 +2,43 @@ import pandas as pd
 
 
 def generate_car_matrix(df)->pd.DataFrame:
-    """
-    Creates a DataFrame  for id combinations.
-
-    Args:
-        df (pandas.DataFrame)
-
-    Returns:
-        pandas.DataFrame: Matrix generated with 'car' values, 
-                          where 'id_1' and 'id_2' are used as indices and columns respectively.
-    """
-    # Write your logic here
-
-    return df
+ matrix_df = df.pivot(index='id_1', columns='id_2', values='car').fillna(0)
+# Set the diagonal values to 0
+for col in matrix_df.columns:
+    matrix_df.loc[matrix_df.index == col, col] = 0
+    matrix_df
 
 
 def get_type_count(df)->dict:
-    """
-    Categorizes 'car' values into types and returns a dictionary of counts.
-
-    Args:
-        df (pandas.DataFrame)
-
-    Returns:
-        dict: A dictionary with car types as keys and their counts as values.
-    """
-    # Write your logic here
-
-    return dict()
+    
+def car_category(x):
+    if x<=15:
+        return "low"
+    elif x>15 and x<=25:
+        return "medium"
+    else:
+        return "high"
+df["Car Cat"] = df["car"].apply(lambda x: car_category(x))
+dict1 = df["Car Cat"].value_counts().to_dict()
+{k: v for k, v in sorted(list(dict1.items()))}
 
 
 def get_bus_indexes(df)->list:
-    """
-    Returns the indexes where the 'bus' values are greater than twice the mean.
-
-    Args:
-        df (pandas.DataFrame)
-
-    Returns:
-        list: List of indexes where 'bus' values exceed twice the mean.
-    """
-    # Write your logic here
-
-    return list()
+a = (np.where(df["bus"] > 2*(df["bus"].mean())))[0].tolist()
 
 
 def filter_routes(df)->list:
-    """
-    Filters and returns routes with average 'truck' values greater than 7.
-
-    Args:
-        df (pandas.DataFrame)
-
-    Returns:
-        list: List of route names with average 'truck' values greater than 7.
-    """
-    # Write your logic here
-
-    return list()
+   df_new = df.groupby("route").agg({"truck":"mean"})
+    df_new.index.tolist()
 
 
 def multiply_matrix(matrix)->pd.DataFrame:
-    """
-    Multiplies matrix values with custom conditions.
-
-    Args:
-        matrix (pandas.DataFrame)
-
-    Returns:
-        pandas.DataFrame: Modified matrix with values multiplied based on custom conditions.
-    """
-    # Write your logic here
-
-    return matrix
+  
+   matrix_df = matrix_df.applymap(lambda x: round((x*0.75),1) if x>20 else round((x*1.25),1))
 
 
 def time_check(df)->pd.Series:
-    """
-    Use shared dataset-2 to verify the completeness of the data by checking whether the timestamps for each unique (`id`, `id_2`) pair cover a full 24-hour and 7 days period
-
-    Args:
-        df (pandas.DataFrame)
-
-    Returns:
-        pd.Series: return a boolean series
-    """
-    # Write your logic here
-
-    return pd.Series()
+    df2 = pd.read_csv("./dataset-2.csv")
+    df2 = df2.sort_values(["id","id_2"])
+    df2
+    
